2be19680628939f8e8fbeb388975caa90c808a7a
Anushakm12 <anushakambli12@gmail.com>
2020-04-12 12:24:03 +0530

    first commit

---
+++ nodejs/nodejs/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/CurdDB',(err) =>
+{
+    if(!err)
+        console.log('MongoDB connection successed..');
+    else
+        console.log('error in db connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
